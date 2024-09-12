bfe3f82f01fb381dc6cf51cfbc05800f7831a391
nicole <nikolet0999@gmail.com>
2024-09-12 14:47:20 +0300

    fix: CloudFrontDistribution DomainName

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name stack \
+aws cloudformation create-stack --stack-name name \
 --template-body file://cloudfront-s3.yml --region us-east-1 \
---parameters ParameterKey=DomainName,ParameterValue=nikol-test-bucket-295640
+--parameters ParameterKey=DomainName,ParameterValue=nikol-test-bucket-924754321
 
 aws cloudformation wait stack-create-complete --stack-name stack --region us-east-1 
 
-aws s3 cp ./www/ s3://nikol-test-bucket-295640 --recursive
+aws s3 cp ./www/ s3://nikol-test-bucket-924754321 --recursive
