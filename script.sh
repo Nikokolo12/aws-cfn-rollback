5efca2aa4021a5920dc7afbe9635267e604642d0
nicole <nikolet0999@gmail.com>
2024-09-13 16:39:34 +0300

    add: certs, hosted zone

--- script.sh
+++ script.sh
@@ -2,8 +2,9 @@
 
 aws cloudformation create-stack --stack-name stack \
 --template-body file://cloudfront-s3.yml --region us-east-1 \
---parameters ParameterKey=DomainName,ParameterValue=nikol-test-bucket-1345451
+--parameters ParameterKey=DomainName,ParameterValue=nikol-test-bucket-1345451 \
+             ParameterKey=WebSiteDomain,ParameterValue=websitetest-bucket.zapto.org 
 
 aws cloudformation wait stack-create-complete --stack-name stack --region us-east-1 
 
-aws s3 cp ./www/ s3://nikol-test-bucket-1345451 --recursive
+#aws s3 cp ./www/ s3://nikol-test-bucket-1345451 --recursive
